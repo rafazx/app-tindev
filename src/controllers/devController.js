f965553ed7bc238c84f123bbd4464be3c41b81d3
Rafael Wogel <rwogel@Mac-mini-de-Tasken.local>
2019-08-12 10:48:34 -0300

    tratamento de valores vazios

--- src/controllers/devController.js
+++ src/controllers/devController.js
@@ -18,6 +18,7 @@ module.exports = {
     },
 
     async store(req, res) {
+        try {
         const userName = req.body.userName;
 
         const userExists = await Dev.findOne( { user: userName });
@@ -28,15 +29,20 @@ module.exports = {
         
         const response = await axios.get(`https://api.github.com/users/${userName}`)
 
-        const { name, bio, avatar_url : avatar } = response.data;
-        
+        const name = response.data.name ? response.data.name : userName;
+        const bio = response.data.bio ? response.data.bio : 'Sem bio';
+        const avatar = response.data.avatar_url ? response.data.avatar_url : 'Sem avatar';
         const dev = await Dev.create({
             name,
             bio,
             user : userName,
             avatar
         })
-
         return res.json(dev);
+
+
+    }catch(error){
+        throw new Error( res.status(500).send(error) );
+    }
     }
 };
